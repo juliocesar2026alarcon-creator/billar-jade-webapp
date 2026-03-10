43bd45faaacf1afdcb0043c8a2d8771db29bba7c
juliocesar2026alarcon-creator <juliocesar2026alarcon@gmail.com>
2026-03-10 09:09:33 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
-
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Genera source maps para diagnosticar errores en producción y evitar pantallas en blanco
 export default defineConfig({
   plugins: [react()],
-  build: {
-    sourcemap: true
-  }
 })
