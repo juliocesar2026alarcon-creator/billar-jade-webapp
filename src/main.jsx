c83ddb872129e8ff7406857e04f0de81eba7a69f
juliocesar2026alarcon-creator <juliocesar2026alarcon@gmail.com>
2026-03-10 10:02:14 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,6 @@
-
+// src/main.jsx
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import ErrorBoundary from './ui/ErrorBoundary.jsx'
 
-function Root() {
-  return (
-    <ErrorBoundary>
-      <App />
-    </ErrorBoundary>
-  )
-}
-
-createRoot(document.getElementById('root')).render(<Root />)
+createRoot(document.getElementById('root')).render(<App />)
